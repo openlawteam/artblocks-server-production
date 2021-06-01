901b30aa7ef354e03007cf39ada9fce88678d7b7
Craig Blake <craigwblake@gmail.com>
2021-06-01 09:03:14 -0400

    Fix missing semicolon.

--- index.js
+++ index.js
@@ -42,14 +42,14 @@ const API_KEY = process.env.INFURA_KEY || "e8eb764fee7a447889f1ee79d2f25934";
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.OSS_ACCESS_KEY,
-  secretAccessKey: process.env.OSS_SECRET_KEY
+  secretAccessKey: process.env.OSS_SECRET_KEY;
 });
 
 const currentNetwork = "mainnet";
 const mediaUrl =
   currentNetwork === "mainnet"
     ? process.env.MEDIA_URL_MAINNET
-    : process.env.MEDIA_URL_RINKEBY
+    : process.env.MEDIA_URL_RINKEBY;
 const curatedProjects =
   currentNetwork === "mainnet"
     ? []
